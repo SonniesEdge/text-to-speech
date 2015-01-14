15c3cf11ab87803515866a63b7445d001af310df
Charlie Owen <charlie.owen@bbc.co.uk>
2015-01-14 12:35:42 +0000

    Basic integration

--- js/voices.js
+++ js/voices.js
@@ -13,7 +13,8 @@
 
     var u = new SpeechSynthesisUtterance();
     // bedtimestory = document.getElementById("story").innerText; // get main article text
-    bedtimestory = document.getElementsByClassName('story-body__inner').innerText;
+    bedtimestory = document.getElementsByClassName('story-body__inner');
+    console.log(bedtimestory);
     // bedtimestory = "I'm a little teapot, short and stout. Here's my handle, here's my spout."
     u.text = bedtimestory;
 
